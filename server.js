06c6aba753ace4194741ce4c301a2238bd1403d5
Jewel Hasan <jewel.hasan@cg-bd.com>
2026-06-08 13:38:44 +0600

    server.js updated

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ import authRoutes from "./routes/authRoutes.js";
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" })); // ✅ increase from default 1mb
+app.use(express.urlencoded({ limit: "10mb", extended: true })); // ✅ add this too
 
 connectDB();
 
